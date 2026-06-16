2fac2276675472f8ea84669ad5478a32f810615c
Ikhlaq <ikhlaqahmad881@gmail.com>
2026-06-16 18:32:50 +0500

    chore: clear all build errors and type mismatches permanently

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['pdfkit'],
+  experimental: {
+    serverComponentsExternalPackages: ['pdfkit', '@sparticuz/chromium'],
+  },
 };
 
-module.exports = nextConfig;s
+module.exports = nextConfig;
